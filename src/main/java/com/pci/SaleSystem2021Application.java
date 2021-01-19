218b0f790c39e555397f6319aacd62147f07a01a
kzhk9 <kzhk9@192.168.11.6>
2021-01-19 13:26:33 +0900

    Form排除版

---
+++ src/main/java/com/pci/SaleSystem2021Application.java
@@ -0,0 +1,13 @@
+package com.pci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleSystem2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaleSystem2021Application.class, args);
+	}
+
+}
